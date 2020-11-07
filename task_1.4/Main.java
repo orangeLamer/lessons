a990e54d82a6ef9fc91fb1984100df4a07730467
orangeLamer <74054120+orangeLamer@users.noreply.github.com>
2020-11-07 21:41:47 +0300

    Update Main.java

--- task_1.4/Main.java
+++ task_1.4/Main.java
@@ -1,25 +1,12 @@
-import java.util.Scanner;
-
 public class Main {
-
     public static void main(String[] args) {
-    
-        double tf,tc_d,r,t,y;
-
-        t = 9;
-        y = 5;
-
-        r = t / y;
-
-        Scanner scan = new Scanner(System.in);
-        int tc = scan.nextInt();
-        tc_d = tc;
-
-        tf =  r * tc_d + 32;
-        System.out.println(tf);
-    
-    
-    
+        int number = 25;
+        //напишите тут ваш код
+        int result;
+        
+        result = number * number;
+       
+        System.out.println(result);
+        
     }
-    
 }
