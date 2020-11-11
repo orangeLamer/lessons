3647015c12d968446eeae65e5c16406976c59056
orangeLamer <74054120+orangeLamer@users.noreply.github.com>
2020-11-11 20:33:28 +0300

    Create Main.java

---
+++ task_2.8/Main.java
@@ -0,0 +1,21 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        int[] arr = {2, 3, 4, 5};
+        int resultt = arr[0];
+
+
+
+        for (int i = 1; i < arr.length; i++){
+
+            resultt *= arr[i];  
+
+        }
+
+        System.out.println(resultt);
+    }
+
+}
