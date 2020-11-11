589260ed9544ca914e66847f4cf3fcf253c304b9
orangeLamer <74054120+orangeLamer@users.noreply.github.com>
2020-11-11 20:19:24 +0300

    Create Main.java

---
+++ task_2.3/Main.java
@@ -0,0 +1,18 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+
+       int a = 0;
+
+       for (int i=0;i<=10;i++){
+
+             for(int j=0;j<a;j++){
+                System.out.print(8);
+             }
+          a++;
+          System.out.println("");
+          
+       }
+    }
+}
