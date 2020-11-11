3ad6ef703481431c0447823633b91a3c076cd83a
orangeLamer <74054120+orangeLamer@users.noreply.github.com>
2020-11-11 20:39:25 +0300

    Update Main.java

--- task_2.9/Main.java
+++ task_2.9/Main.java
@@ -2,21 +2,29 @@
 public class Main {
 
     public static void main(String[] args) {
-	
 
-        int[] arr = {2, 3, 4, 5};
-        int resultt = arr[0];
+	// write your code here
+    	int chek_num = 5;
+    	boolean b_bul = true;
+
+    	int arr[] = {7,4,7,0,2,1,2,88,99,5,3,777,9};
+
+    	for (int i = 0; i < arr.length; i++) {
+
+    		if (chek_num == arr[i]) {
 
 
+				System.out.println("да");
 
-        for (int i = 1; i < arr.length; i++){
+				b_bul = false;
+			}
+    	}
 
-            resultt *= arr[i];  
+    	if (b_bul) {
+    		
+    		System.out.println("нет");
+    	}
 
-            
-        }
-
-        System.out.println(resultt);
     }
-
 }
+
