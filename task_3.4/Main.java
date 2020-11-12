023075c7ecf09d325176db0cf3aff082348de033
orangeLamer <74054120+orangeLamer@users.noreply.github.com>
2020-11-12 15:59:11 +0300

    Create Main.java

---
+++ task_3.4/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+
+        super10(27,5);
+
+
+    }
+    static void super10 (int num1, int num2){
+        if ((num1 + num2) > 10)
+            System.out.println(true);
+        else
+            System.out.println(false);
+    }
+}
